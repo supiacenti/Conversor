68ef4bbca066eea6312a8f4840da1b5f54f7d515
Suellen Piacenti <84042162+supiacenti@users.noreply.github.com>
2021-06-09 19:44:17 -0300

    segundo commit

--- Conversor de Bases/js/conversor.js
+++ Conversor de Bases/js/conversor.js
@@ -1,20 +1,53 @@
+const vb = document.getElementById("valorb");
+const vo = document.getElementById("valoro");
+const vh = document.getElementById("valorh");
+const vd = document.getElementById("valord");
 var r = document.getElementById("resposta");
-const valor = document.getElementById("valor");
 
-document.querySelector("#db").addEventListener("click", function(){
-    const value = valor.value;
-    let dec = 0;
-    dec = (value >>> 0).toString(2);
-    
-    r.innerHTML = '<p class="r">' + value + ' para binário é igual a: <b>' + dec + '</b></p>'
-    
-});
+$("#bina").show();
+$("#octa").hide();
+$("#hexa").hide();
+$("#deci").hide();
+$("#bin").click(function(){
+    $("#bina").show();
+    $("#octa").hide();
+    $("#hexa").hide();
+    $("#deci").hide();
+})
 document.querySelector("#bd").addEventListener("click", function(){
-    const value = valor.value;
+    const value = vb.value;
     let dec = 0;
     dec = parseInt(value, 2);
     
     r.innerHTML = '<p class="r">' + value + ' para decimal é igual a: <b>' + dec + '</b></p>';
+});
+
+$("#oct").click(function(){
+    $("#bina").hide();
+    $("#octa").show();
+    $("#hexa").hide();
+    $("#deci").hide();
+})
+
+$("#hex").click(function(){
+    $("#bina").hide();
+    $("#octa").hide();
+    $("#hexa").show();
+    $("#deci").hide();
+})
+
+$("#dec").click(function(){
+    $("#bina").hide();
+    $("#octa").hide();
+    $("#hexa").hide();
+    $("#deci").show();
+})
+document.querySelector("#db").addEventListener("click", function(){
+    const value = vd.value;
+    let dec = 0;
+    dec = (value >>> 0).toString(2);
+    
+    r.innerHTML = '<p class="r">' + value + ' para binário é igual a: <b>' + dec + '</b></p>';
 });
 
 document.querySelector("#again").addEventListener("click", function(){
